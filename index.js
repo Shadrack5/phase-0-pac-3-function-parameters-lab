c2c915f065b320032dc7f58b85125fb2de970d14
Shadrack5 <shadrackkibet100@gmail.com>
2023-06-10 18:16:04 +0300

    completed assingment

---
+++ index.js
@@ -0,0 +1,10 @@
+let name = Kev
+function introduction(name){
+function introduction(name = "Kev"){
+    return "Hi, my name is " + name + "."
+}}
+let language = inEmber.js
+function introductionWithLanguage(name,language){
+function introductionWithLanguage(name,language = "inEmber.js"){
+    return "Hi, my name is " + name + " and I am learning to program in " + language + "."
+}}
